21629106b732c35fb8b72ff0d4d45df971ee59c7
Jérémy Barascut <jeremy.barascut@camptocamp.com>
2023-08-21 10:46:03 +0200

    feat(init): add module

---
+++ main.tf
@@ -0,0 +1,50 @@
+# NOTE: the Name used for Redis needs to be globally unique
+resource "azurerm_redis_cache" "this" {
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  redis_version                 = var.redis_version
+  capacity                      = var.capacity
+  family                        = var.family
+  sku_name                      = var.sku_name
+  enable_non_ssl_port           = false
+  public_network_access_enabled = false
+  minimum_tls_version           = var.minimum_tls_version
+  shard_count                   = var.sku_name == "Premium" ? var.cluster_shard_count : 0
+  tags                          = var.tags
+  zones                         = var.zones
+  patch_schedule {
+    day_of_week    = "Sunday"
+    start_hour_utc = 1
+  }
+}
+
+resource "azurerm_private_endpoint" "this" {
+  name                = azurerm_redis_cache.this.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = azurerm_redis_cache.this.name
+    private_connection_resource_id = azurerm_redis_cache.this.id
+    subresource_names              = ["redisCache"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name = "default"
+
+    private_dns_zone_ids = [
+      var.private_dns_zone_id
+    ]
+  }
+}
+
+resource "azurerm_management_lock" "this" {
+  count      = var.instance_lock ? 1 : 0
+  name       = format("%s-mg-lock", azurerm_redis_cache.this.name)
+  scope      = azurerm_redis_cache.this.id
+  lock_level = "CanNotDelete"
+  notes      = "This is a security mechanism to prevent accidental deletion. Deleting a redis instance drops all keys and also backups."
+}
---
+++ variables.tf
@@ -0,0 +1,100 @@
+variable "resource_group_name" {
+  description = "Resource group name."
+  type        = string
+}
+
+variable "location" {
+  description = "Location of resource(s)."
+  type        = string
+}
+
+variable "name" {
+  description = "Name of azure redis cache"
+  type        = string
+}
+
+variable "redis_version" {
+  description = "Redis version"
+  type        = number
+  default     = 6
+}
+
+variable "capacity" {
+  description = " The size of the Redis cache to deploy. Valid values for a SKU family of C (Basic/Standard) are 0, 1, 2, 3, 4, 5, 6, and for P (Premium) family are 1, 2, 3, 4, 5."
+  type        = number
+  default     = 0
+}
+
+variable "family" {
+  description = "Basic=C, Standard=C, Premium=P"
+  type        = string
+  default     = "C"
+}
+
+variable "sku_name" {
+  description = "Redis Cache Sku name. Can be Basic, Standard or Premium"
+  type        = string
+  default     = "Basic"
+}
+
+variable "cluster_shard_count" {
+  description = "Number of cluster shards desired"
+  type        = number
+  default     = 3
+}
+
+variable "minimum_tls_version" {
+  description = "The minimum TLS version"
+  type        = string
+  default     = "1.2"
+}
+
+variable "virtual_network_name" {
+  description = "Virtual Network Name for Private Dns Zone"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+
+}
+variable "private_dns_zone_name" {
+  description = "The name of the private dns zone"
+  type        = string
+  default     = ""
+}
+
+variable "private_dns_zone_id" {
+  description = "The id of the private dns zone"
+  type        = string
+}
+
+variable "patch_schedules" {
+  description = "A list of Patch Schedule, Azure Cache for Redis patch schedule is used to install important software updates in specified time window."
+  default     = []
+  nullable    = false
+  type = list(object({
+    day_of_week        = string
+    start_hour_utc     = optional(string)
+    maintenance_window = optional(string)
+  }))
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "instance_lock" {
+  description = "If true, it’s not possible to remove the azure redis cache"
+  type        = bool
+  default     = true
+}
+
+variable "zones" {
+  description = "(Optional) Specifies a list of Availability Zones in which this Redis Cache should be located. Changing this forces a new Redis Cache to be created."
+  type        = list(any)
+  default     = []
+}
