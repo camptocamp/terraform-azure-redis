14004e8eeb212a8778a2a3b4bd42f6bbd6e1da1b
Jérémy Barascut <jeremy.barascut@camptocamp.com>
2024-04-10 10:03:55 +0200

    feat(network): output private hostname

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "hostname" {
   value = azurerm_redis_cache.this.hostname
 }
 
+output "private_hostname" {
+  value = replace(azurerm_redis_cache.this.hostname, ".redis.cache.windows.net", ".privatelink.redis.cache.windows.net")
+}
+
 output "ssl_port" {
   value = azurerm_redis_cache.this.ssl_port
 }
